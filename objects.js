6efdff6f3a93f0aeadcf483a5f6b1fbc2808938e
ljubica berger <ljubicaberger@gmail.com>
2018-08-21 01:36:18 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,17 @@
+var playlist = {
+  artistName : songTitle,
+};
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  Object.assign({}, plalist, {
+    
+    artistName: ['']
+    
+  })
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.artistName;
+  
+  return playlist;
+}
